52e5f952c60e68d349dc9cf151106ebc4952a978
thiagodemelofarias-spec <thiagodemelo.farias@gmail.com>
2026-05-26 11:14:54 -0300

    Update Distribuição_de_Escolas_por_Região_e_Dependência_Administrativa.js

--- Graficos/Distribuição_de_Escolas_por_Região_e_Dependência_Administrativa.js
+++ Graficos/Distribuição_de_Escolas_por_Região_e_Dependência_Administrativa.js
@@ -1,1 +1,26 @@
-
+var grafico03 = {
+  vl.markBar()
+  .data(dadosRegioes)
+  .encode(
+    vl.x().fieldN("regiao")
+          .title("Região")
+          .sort(["Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul"]),
+    vl.y().fieldQ("escolas")
+          .aggregate("sum")
+          .title("Quantidade de Escolas")
+          .stack(true),
+    vl.color().fieldN("tipo")
+              .title("Tipo de Gestão")
+              .scale({ scheme: "viridis" }),  
+    vl.order().fieldN("tipo")                 
+  )
+  .title({
+    text: "Distribuição de Escolas por Região e Dependência Administrativa",
+    fontSize: 14,
+    fontWeight: "bold"
+  })
+  .width(600)
+  .height(400)
+  .render()
+};
+vegaEmbed('#Distribuição_de_Escolas_por_Região_e_Dependência_Administrativa', grafico03);
